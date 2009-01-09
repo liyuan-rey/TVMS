a47a69d44cbb3d9209d8f14619a7ee6a13d12cbb
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2009-01-09 06:57:04 +0000

    移植SqlHelper适应OleDb数据库
    git-svn-id: svn://localhost/TVMS/TVMS/trunk@13 77a49dfb-f71c-f749-a31f-818bb650a3af

--- code/DBUtility/OleDbHelper.cs
+++ code/DBUtility/OleDbHelper.cs
@@ -7,44 +7,41 @@
 using System;
 using System.Configuration;
 using System.Data;
-using System.Data.SqlClient;
+using System.Data.OleDb;
 using System.Collections;
 
 namespace TVMS.DBUtility {
 
     /// <summary>
-    /// The SqlHelper class is intended to encapsulate high performance, 
-    /// scalable best practices for common uses of SqlClient.
+    /// The OleDbHelper class is intended to encapsulate high performance, 
+    /// scalable best practices for common uses of OleDb.
     /// </summary>
     public abstract class OleDbHelper {
 
         //Database connection strings
-        public static readonly string ConnectionStringLocalTransaction = ConfigurationManager.ConnectionStrings["SQLConnString1"].ConnectionString;
-        public static readonly string ConnectionStringInventoryDistributedTransaction = ConfigurationManager.ConnectionStrings["SQLConnString2"].ConnectionString;
-        public static readonly string ConnectionStringOrderDistributedTransaction = ConfigurationManager.ConnectionStrings["SQLConnString3"].ConnectionString;
-		public static readonly string ConnectionStringProfile = ConfigurationManager.ConnectionStrings["SQLProfileConnString"].ConnectionString;		
+        public static readonly string ConnectionStringLocalTransaction = TVMS.Properties.Settings.Default.SQLConnString;
 		
         // Hashtable to store cached parameters
         private static Hashtable parmCache = Hashtable.Synchronized(new Hashtable());
 
         /// <summary>
-        /// Execute a SqlCommand (that returns no resultset) against the database specified in the connection string 
+        /// Execute a OleDbCommand (that returns no resultset) against the database specified in the connection string 
         /// using the provided parameters.
         /// </summary>
         /// <remarks>
         /// e.g.:  
-        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new OleDbParameter("@prodid", 24));
         /// </remarks>
-        /// <param name="connectionString">a valid connection string for a SqlConnection</param>
+        /// <param name="connectionString">a valid connection string for a OleDbConnection</param>
         /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
         /// <param name="commandText">the stored procedure name or T-SQL command</param>
-        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <param name="commandParameters">an array of OleDbParameters used to execute the command</param>
         /// <returns>an int representing the number of rows affected by the command</returns>
-        public static int ExecuteNonQuery(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters) {
+        public static int ExecuteNonQuery(string connectionString, CommandType cmdType, string cmdText, params OleDbParameter[] commandParameters) {
 
-            SqlCommand cmd = new SqlCommand();
+            OleDbCommand cmd = new OleDbCommand();
 
-            using (SqlConnection conn = new SqlConnection(connectionString)) {
+            using (OleDbConnection conn = new OleDbConnection(connectionString)) {
                 PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
                 int val = cmd.ExecuteNonQuery();
                 cmd.Parameters.Clear();
@@ -53,21 +50,21 @@ namespace TVMS.DBUtility {
         }
 
         /// <summary>
-        /// Execute a SqlCommand (that returns no resultset) against an existing database connection 
+        /// Execute a OleDbCommand (that returns no resultset) against an existing database connection 
         /// using the provided parameters.
         /// </summary>
         /// <remarks>
         /// e.g.:  
-        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new OleDbParameter("@prodid", 24));
         /// </remarks>
         /// <param name="conn">an existing database connection</param>
         /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
         /// <param name="commandText">the stored procedure name or T-SQL command</param>
-        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <param name="commandParameters">an array of OleDbParameters used to execute the command</param>
         /// <returns>an int representing the number of rows affected by the command</returns>
-        public static int ExecuteNonQuery(SqlConnection connection, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters) {
+        public static int ExecuteNonQuery(OleDbConnection connection, CommandType cmdType, string cmdText, params OleDbParameter[] commandParameters) {
 
-            SqlCommand cmd = new SqlCommand();
+            OleDbCommand cmd = new OleDbCommand();
 
             PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
             int val = cmd.ExecuteNonQuery();
@@ -76,20 +73,20 @@ namespace TVMS.DBUtility {
         }
 
         /// <summary>
-        /// Execute a SqlCommand (that returns no resultset) using an existing SQL Transaction 
+        /// Execute a OleDbCommand (that returns no resultset) using an existing SQL Transaction 
         /// using the provided parameters.
         /// </summary>
         /// <remarks>
         /// e.g.:  
-        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new OleDbParameter("@prodid", 24));
         /// </remarks>
         /// <param name="trans">an existing sql transaction</param>
         /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
         /// <param name="commandText">the stored procedure name or T-SQL command</param>
-        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <param name="commandParameters">an array of OleDbParameters used to execute the command</param>
         /// <returns>an int representing the number of rows affected by the command</returns>
-        public static int ExecuteNonQuery(SqlTransaction trans, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters) {
-            SqlCommand cmd = new SqlCommand();
+        public static int ExecuteNonQuery(OleDbTransaction trans, CommandType cmdType, string cmdText, params OleDbParameter[] commandParameters) {
+            OleDbCommand cmd = new OleDbCommand();
             PrepareCommand(cmd, trans.Connection, trans, cmdType, cmdText, commandParameters);
             int val = cmd.ExecuteNonQuery();
             cmd.Parameters.Clear();
@@ -97,28 +94,28 @@ namespace TVMS.DBUtility {
         }
 
         /// <summary>
-        /// Execute a SqlCommand that returns a resultset against the database specified in the connection string 
+        /// Execute a OleDbCommand that returns a resultset against the database specified in the connection string 
         /// using the provided parameters.
         /// </summary>
         /// <remarks>
         /// e.g.:  
-        ///  SqlDataReader r = ExecuteReader(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        ///  OleDbDataReader r = ExecuteReader(connString, CommandType.StoredProcedure, "PublishOrders", new OleDbParameter("@prodid", 24));
         /// </remarks>
-        /// <param name="connectionString">a valid connection string for a SqlConnection</param>
+        /// <param name="connectionString">a valid connection string for a OleDbConnection</param>
         /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
         /// <param name="commandText">the stored procedure name or T-SQL command</param>
-        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
-        /// <returns>A SqlDataReader containing the results</returns>
-        public static SqlDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters) {
-            SqlCommand cmd = new SqlCommand();
-            SqlConnection conn = new SqlConnection(connectionString);
+        /// <param name="commandParameters">an array of OleDbParameters used to execute the command</param>
+        /// <returns>A OleDbDataReader containing the results</returns>
+        public static OleDbDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params OleDbParameter[] commandParameters) {
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbConnection conn = new OleDbConnection(connectionString);
 
             // we use a try/catch here because if the method throws an exception we want to 
             // close the connection throw code, because no datareader will exist, hence the 
             // commandBehaviour.CloseConnection will not work
             try {
                 PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
-                SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                OleDbDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
                 cmd.Parameters.Clear();
                 return rdr;
             }
@@ -129,22 +126,22 @@ namespace TVMS.DBUtility {
         }
 
         /// <summary>
-        /// Execute a SqlCommand that returns the first column of the first record against the database specified in the connection string 
+        /// Execute a OleDbCommand that returns the first column of the first record against the database specified in the connection string 
         /// using the provided parameters.
         /// </summary>
         /// <remarks>
         /// e.g.:  
-        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new OleDbParameter("@prodid", 24));
         /// </remarks>
-        /// <param name="connectionString">a valid connection string for a SqlConnection</param>
+        /// <param name="connectionString">a valid connection string for a OleDbConnection</param>
         /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
         /// <param name="commandText">the stored procedure name or T-SQL command</param>
-        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <param name="commandParameters">an array of OleDbParameters used to execute the command</param>
         /// <returns>An object that should be converted to the expected type using Convert.To{Type}</returns>
-        public static object ExecuteScalar(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters) {
-            SqlCommand cmd = new SqlCommand();
+        public static object ExecuteScalar(string connectionString, CommandType cmdType, string cmdText, params OleDbParameter[] commandParameters) {
+            OleDbCommand cmd = new OleDbCommand();
 
-            using (SqlConnection connection = new SqlConnection(connectionString)) {
+            using (OleDbConnection connection = new OleDbConnection(connectionString)) {
                 PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
                 object val = cmd.ExecuteScalar();
                 cmd.Parameters.Clear();
@@ -153,21 +150,21 @@ namespace TVMS.DBUtility {
         }
 
         /// <summary>
-        /// Execute a SqlCommand that returns the first column of the first record against an existing database connection 
+        /// Execute a OleDbCommand that returns the first column of the first record against an existing database connection 
         /// using the provided parameters.
         /// </summary>
         /// <remarks>
         /// e.g.:  
-        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new SqlParameter("@prodid", 24));
+        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new OleDbParameter("@prodid", 24));
         /// </remarks>
         /// <param name="conn">an existing database connection</param>
         /// <param name="commandType">the CommandType (stored procedure, text, etc.)</param>
         /// <param name="commandText">the stored procedure name or T-SQL command</param>
-        /// <param name="commandParameters">an array of SqlParamters used to execute the command</param>
+        /// <param name="commandParameters">an array of OleDbParameters used to execute the command</param>
         /// <returns>An object that should be converted to the expected type using Convert.To{Type}</returns>
-        public static object ExecuteScalar(SqlConnection connection, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters) {
+        public static object ExecuteScalar(OleDbConnection connection, CommandType cmdType, string cmdText, params OleDbParameter[] commandParameters) {
 
-            SqlCommand cmd = new SqlCommand();
+            OleDbCommand cmd = new OleDbCommand();
 
             PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
             object val = cmd.ExecuteScalar();
@@ -179,8 +176,8 @@ namespace TVMS.DBUtility {
         /// add parameter array to the cache
         /// </summary>
         /// <param name="cacheKey">Key to the parameter cache</param>
-        /// <param name="cmdParms">an array of SqlParamters to be cached</param>
-        public static void CacheParameters(string cacheKey, params SqlParameter[] commandParameters) {
+        /// <param name="cmdParms">an array of OleDbParameters to be cached</param>
+        public static void CacheParameters(string cacheKey, params OleDbParameter[] commandParameters) {
             parmCache[cacheKey] = commandParameters;
         }
 
@@ -188,17 +185,17 @@ namespace TVMS.DBUtility {
         /// Retrieve cached parameters
         /// </summary>
         /// <param name="cacheKey">key used to lookup parameters</param>
-        /// <returns>Cached SqlParamters array</returns>
-        public static SqlParameter[] GetCachedParameters(string cacheKey) {
-            SqlParameter[] cachedParms = (SqlParameter[])parmCache[cacheKey];
+        /// <returns>Cached OleDbParameters array</returns>
+        public static OleDbParameter[] GetCachedParameters(string cacheKey) {
+            OleDbParameter[] cachedParms = (OleDbParameter[])parmCache[cacheKey];
 
             if (cachedParms == null)
                 return null;
 
-            SqlParameter[] clonedParms = new SqlParameter[cachedParms.Length];
+            OleDbParameter[] clonedParms = new OleDbParameter[cachedParms.Length];
 
             for (int i = 0, j = cachedParms.Length; i < j; i++)
-                clonedParms[i] = (SqlParameter)((ICloneable)cachedParms[i]).Clone();
+                clonedParms[i] = (OleDbParameter)((ICloneable)cachedParms[i]).Clone();
 
             return clonedParms;
         }
@@ -206,13 +203,13 @@ namespace TVMS.DBUtility {
         /// <summary>
         /// Prepare a command for execution
         /// </summary>
-        /// <param name="cmd">SqlCommand object</param>
-        /// <param name="conn">SqlConnection object</param>
-        /// <param name="trans">SqlTransaction object</param>
+        /// <param name="cmd">OleDbCommand object</param>
+        /// <param name="conn">OleDbConnection object</param>
+        /// <param name="trans">OleDbTransaction object</param>
         /// <param name="cmdType">Cmd type e.g. stored procedure or text</param>
         /// <param name="cmdText">Command text, e.g. Select * from Products</param>
-        /// <param name="cmdParms">SqlParameters to use in the command</param>
-        private static void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType cmdType, string cmdText, SqlParameter[] cmdParms) {
+        /// <param name="cmdParms">OleDbParameters to use in the command</param>
+        private static void PrepareCommand(OleDbCommand cmd, OleDbConnection conn, OleDbTransaction trans, CommandType cmdType, string cmdText, OleDbParameter[] cmdParms) {
 
             if (conn.State != ConnectionState.Open)
                 conn.Open();
@@ -226,7 +223,7 @@ namespace TVMS.DBUtility {
             cmd.CommandType = cmdType;
 
             if (cmdParms != null) {
-                foreach (SqlParameter parm in cmdParms)
+                foreach (OleDbParameter parm in cmdParms)
                     cmd.Parameters.Add(parm);
             }
         }
